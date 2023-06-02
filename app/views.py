2c4e2926208f797c32c54d49e3781ef0cede7889
Basinenimanasa <manasaboya794@gmail.com>
2023-06-02 23:11:50 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def sanju(request):
+    return render(request,'sanju.html')
+def sai(request):
+    return render(request,'sai.html')
